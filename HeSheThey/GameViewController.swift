07e9f4ce262aae4bedc75c639ac780fd8b1f463f
Josh Rubin <joshrubin2014@gmail.com>
2023-03-11 21:19:17 -0500

    can rotate through pictures in gameview and first sound plays

--- HeSheThey/GameViewController.swift
+++ HeSheThey/GameViewController.swift
@@ -12,28 +12,29 @@ class GameViewController: UIViewController, AVAudioPlayerDelegate {
     var gameView: UIView!
     var skipInstructionsButton: UIButton!
     var beginButton: UIButton!
-    
-  
-    
+    var rightButton: UIButton!
+    var leftButton: UIButton!
+    var middleButton: UIButton!
+    var index: Int = 0
+    var randomIndex: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         playInstructionsSound()
-        let beginButton = UIButton(frame: CGRect(x: self.view.frame.size.width/2 - 50, y: self.view.frame.size.height - 64, width: 100, height: 44))
+    
+        
+        
+
+        beginButton = UIButton(frame: CGRect(x: self.view.frame.size.width/2 - 50, y: self.view.frame.size.height - 64, width: 100, height: 44))
         beginButton.setTitle("Begin!", for: .normal)
         beginButton.titleLabel?.font = UIFont(name: "ChalkboardSE-Regular", size: 24)
         beginButton.setTitleColor(UIColor.blue, for: .normal)
     
         beginButton.addTarget(self, action: #selector(_revealGameView), for: .touchUpInside)
         self.view.addSubview(beginButton)
-    
-        let skipInstructionsButton = UIButton(frame: CGRect(x: self.view.frame.size.width - 310, y: self.view.frame.size.height - 64, width: 310, height: 44))
-        skipInstructionsButton.setTitle("Skip Instructions", for: .normal)
-        skipInstructionsButton.titleLabel?.font = UIFont(name: "ChalkboardSE-Regular", size: 24)
-        skipInstructionsButton.setTitleColor(UIColor.red, for: .normal)
-        skipInstructionsButton.addTarget(self, action: #selector(_revealGameView), for: .touchUpInside)
-        skipInstructionsButton.isHidden = false
-        self.view.addSubview(skipInstructionsButton)
+        
+        
+     
        
         let homeButton = UIButton(frame: CGRect(x: 15, y: 15, width: 44, height: 44))
         homeButton.setImage(UIImage(named: "bluehome"), for: .normal)
@@ -57,6 +58,7 @@ class GameViewController: UIViewController, AVAudioPlayerDelegate {
                     let audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
                     audioPlayer.volume = 1.0
                     audioPlayer.play()
+
                 } catch {
                     print("Error")
                 }
@@ -72,16 +74,24 @@ class GameViewController: UIViewController, AVAudioPlayerDelegate {
 
         let leftButtonX = (view.frame.width / 2.7) - (buttonWidth + (spaceBetweenButtons / 2))
         let leftButtonY = (view.frame.height / 2) - (buttonHeight / 2)
-        let leftButton = UIButton(frame: CGRect(x: leftButtonX, y: leftButtonY, width: buttonWidth, height: buttonHeight))
+         leftButton = UIButton(frame: CGRect(x: leftButtonX, y: leftButtonY, width: buttonWidth, height: buttonHeight))
 
         let middleButtonX = view.frame.width / 2.7
         let middleButtonY = (view.frame.height / 2) - (buttonHeight / 2)
-        let middleButton = UIButton(frame: CGRect(x: middleButtonX, y: middleButtonY, width: buttonWidth, height: buttonHeight))
+         middleButton = UIButton(frame: CGRect(x: middleButtonX, y: middleButtonY, width: buttonWidth, height: buttonHeight))
 
         let rightButtonX = (view.frame.width / 2.7) + (buttonWidth + (spaceBetweenButtons / 2))
         let rightButtonY = (view.frame.height / 2) - (buttonHeight / 2)
-        let rightButton = UIButton(frame: CGRect(x: rightButtonX, y: rightButtonY, width: buttonWidth, height: buttonHeight))
+         rightButton = UIButton(frame: CGRect(x: rightButtonX, y: rightButtonY, width: buttonWidth, height: buttonHeight))
         
+        let firstFlashcard = flashcards[index]
+        let leftImage = UIImage(named: firstFlashcard.0)
+        let middleImage = UIImage(named: firstFlashcard.1)
+        let rightImage = UIImage(named: firstFlashcard.2)
+        
+        leftButton.setImage(leftImage, for: .normal)
+        middleButton.setImage(middleImage, for: .normal)
+        rightButton.setImage(rightImage, for: .normal)
         
         leftButton.backgroundColor = .white
         leftButton.layer.borderWidth = 2
@@ -95,6 +105,21 @@ class GameViewController: UIViewController, AVAudioPlayerDelegate {
         rightButton.layer.borderWidth = 2
         rightButton.layer.borderColor = UIColor.black.cgColor
         
+        
+      
+        let switchButton = UIButton(frame: CGRect(x: rightButton.frame.maxX + 10, y: rightButton.frame.minY, width: 50, height: 50))
+        switchButton.setTitle("Next", for: .normal)
+        switchButton.backgroundColor = .white
+        switchButton.layer.borderWidth = 2
+        switchButton.layer.borderColor = UIColor.black.cgColor
+
+//        switchButton.addTarget(self, action: #selector(switchFlashcards), for: .touchUpInside)
+
+//       let initialButtons = [leftButton, middleButton, rightButton]
+        leftButton.contentMode = .scaleAspectFit
+        middleButton.contentMode = .scaleAspectFit
+        rightButton.contentMode = .scaleAspectFit
+        switchButton.addTarget(self, action: #selector(nextFlashcard), for: .touchUpInside)
         gameView = UIView(frame: self.view.frame)
         gameView.backgroundColor = UIColor.white
         gameView.isHidden = true
@@ -102,6 +127,7 @@ class GameViewController: UIViewController, AVAudioPlayerDelegate {
         gameView.addSubview(leftButton)
         gameView.addSubview(middleButton)
         gameView.addSubview(rightButton)
+        gameView.addSubview(switchButton)
         self.view.addSubview(gameView)
         
     }
@@ -114,23 +140,49 @@ class GameViewController: UIViewController, AVAudioPlayerDelegate {
             try AVAudioSession.sharedInstance().setActive(true)
             
             audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
+     
             guard let audioPlayer = audioPlayer else { return }
             audioPlayer.delegate = self
-    
             audioPlayer.play()
             
         } catch let error {
             print(error.localizedDescription)
         }
+        
+        
     }
+    
 
+
+//
+//    @objc func switchFlashcards(_ sender: UIButton) {
+//            let randomIndex = Int.random(in: 0 ..< flashcards.count)
+//            let buttons = [leftButton, middleButton, rightButton]
+//
+//            index = (index + 1) % flashcards.count
+//        }
+    
+    
         @objc func soundButtonClicked(sender: UIButton!) {
         if !(audioPlayer?.isPlaying ?? false) {
                     playInstructionsSound()
                 }
         }
-    
+ 
+    @objc func nextFlashcard(){
+        index += 1
+        if index == flashcards.count{
+            index = 0
+        }
+        let nextFlashcard = flashcards[index]
+        let leftImage = UIImage(named: nextFlashcard.0)
+        let middleImage = UIImage(named: nextFlashcard.1)
+        let rightImage = UIImage(named: nextFlashcard.2)
+        
+        leftButton.setImage(leftImage, for: .normal)
+        middleButton.setImage(middleImage, for: .normal)
+        rightButton.setImage(rightImage, for: .normal)
+    }
     @objc func _dismiss(){
         self.dismiss(animated: true)
     }
@@ -138,12 +190,15 @@ class GameViewController: UIViewController, AVAudioPlayerDelegate {
     @objc func _revealGameView(){
         gameView.isHidden = false
         audioPlayer?.stop()
-    }
-    
-    
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        beginButton?.isHidden = false
-        skipInstructionsButton?.isHidden = true
+//        if let soundPath = Bundle.main.path(forResource: flashcards[0].0, ofType: "wav") {
+//            let soundUrl = URL(fileURLWithPath: soundPath)
+//            do {
+//                audioPlayer = try AVAudioPlayer(contentsOf: soundUrl)
+//                audioPlayer?.play()
+//            } catch {
+//                print("Error")
+//            }
+//        }
     }
  
 }
---
+++ HeSheThey/flashcards.swift
@@ -0,0 +1,14 @@
+//
+//  flashcards.swift
+//  HeSheThey
+//
+//  Created by Josh Rubin on 3/11/23.
+//
+
+import Foundation
+
+public let flashcards: [(String, String, String)] = [
+    ("eatingHe", "eatingShe","eatingThey"),
+    ("jumpingHe", "jumpingShe","jumpingThey"),
+    ("drinkingHe", "drinkingShe","drinkingThey")
+]
